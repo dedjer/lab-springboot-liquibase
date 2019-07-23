96ce05bf4091160ab8e07668b53d3fe0eeb409d0
DJ Garcia <dedjer@gmail.com>
2019-07-23 12:26:59 -0500

    Initial commit

---
+++ src/main/java/com/example/labspringbootliquibase/LabSpringbootLiquibaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.labspringbootliquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabSpringbootLiquibaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabSpringbootLiquibaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/labspringbootliquibase/LabSpringbootLiquibaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.labspringbootliquibase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabSpringbootLiquibaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
